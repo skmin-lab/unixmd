ebc6a6ac750f6578dad7d62c6d8568d728714406
tikimtikim <tikim@unist.ac.kr>
2019-12-04 12:27:48 +0900

    TDDFT->DFT & Indentation

--- src/bo/turbomole/turbomole.py
+++ src/bo/turbomole/turbomole.py
@@ -6,16 +6,14 @@ class Turbomole(BO_calculator):
     """
     def __init__(self, functional, basis_set, memory, qm_path, nthreads, version):
       #
-      self.functional = functional
-      self.basis_set = basis_set
-      
-      self.memory = memory
-      self.qm_path = qm_path
-      self.nthreads = nthreads
-      self.version = version
-      
-      if (self.version != 6.4):
-        raise ValueError ("TURBOMOMLE6.4 is ONLY available!")
-
-      
+        self.functional = functional
+        self.basis_set = basis_set
+        
+        self.memory = memory
+        self.qm_path = qm_path
+        self.nthreads = nthreads
+        self.version = version
+        
+        if (self.version != 6.4):
+            raise ValueError ("TURBOMOMLE6.4 is ONLY available!")
       
