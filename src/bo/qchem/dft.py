2eee39ab2534f99bdd59909f14a82a429f9d54b1
tikimtikim <tikim@unist.ac.kr>
2020-05-06 16:50:34 +0900

    implement extract_BO

--- src/bo/qchem/dft.py
+++ src/bo/qchem/dft.py
@@ -24,7 +24,7 @@ class DFT(QChem):
         self.write_xyz(molecule)
         self.get_input(molecule, bo_list, calc_force_only)
         self.run_QM(base_dir, istep, bo_list)
-       # self.extract_BO(molecule, bo_list, calc_force_only)
+        self.extract_BO(molecule, bo_list, calc_force_only)
        # self.move_dir(base_dir)
 
     def get_input(self, molecule, bo_list, calc_force_only):
@@ -47,8 +47,8 @@ class DFT(QChem):
         input_molecule += "$end\n\n"
         input_qc += input_molecule
 
-        # Job control
-        input_rem = textwrap.dedent(f"""\
+        # Job control to calculate NAC
+        input_nac = textwrap.dedent(f"""\
         $rem
         JOBTYPE  SP
         METHOD {self.functional}
@@ -58,18 +58,38 @@ class DFT(QChem):
         CIS_TRIPLETS FALSE
         CALC_NAC TRUE
         CIS_DER_NUMSTATE {molecule.nst}
+        !CIS_STATE_DERIV {bo_list[0]}
         $end
 
         $derivative_coupling
         running state is {bo_list[0]}
         """)
 
-        for ist in range(molecule.nst + 1):
-            input_rem += f"{ist}  "
-        input_rem += "\n$end\n\n"
+        for ist in range(molecule.nst):
+            input_nac += f"{ist}  "
+        input_nac += "\n$end\n\n"
 
-        input_qc += input_rem
+        input_qc += input_nac
   
+        # Job control to calculate force
+        input_force = textwrap.dedent(f"""\
+
+        @@@
+
+        $molcule
+        read
+        $end
+
+        $rem
+        JOBTYPE force
+        METHOD {self.functional}
+        BASIS {self.basis_set}
+        SCF_GUESS read
+        $end
+        """)
+
+        input_qc += input_force
+
         file_name = "qchem.in"
         with open(file_name, "w") as f:
             f.write(input_qc)
@@ -91,9 +111,79 @@ class DFT(QChem):
             log_step = f"log.{istep + 1}.{bo_list[0]}"
             shutil.copy("log", os.path.join(tmp_dir, log_step))
 
-        # run QChem
+        # Run QChem
         os.system(qm_exec_command)
 
     def extract_BO(self, molecule, bo_list, calc_force_only):
         """ Read the output files to get BO information
         """
+        #os.system('cp /home/tikim/11-program-test/01-Qchem/04-NAC-test/nac.out ./log')
+        #os.system('cp /home/tikim/11-program-test/01-Qchem/05-force-NAC/nac-force-es.out ./log')
+        os.system('cp /home/tikim/11-program-test/01-Qchem/05-force-NAC/qchem.out ./log')
+        file_name = "log"
+        with open(file_name, "r") as f:
+            log = f.read()
+
+        if (not calc_force_only):
+            for states in molecule.states:
+                states.energy = 0.
+        
+            # Ground state energy
+            energy = re.findall('Total energy in the final basis set =\s*([-]*\S*)', log)
+            energy = np.array(energy)
+            energy = energy.astype(float)
+            molecule.states[0].energy = energy[0]
+
+            # Excited state energy
+            if (molecule.nst > 1):
+                energy = re.findall('Total energy for state\s*\d*.\s*([-]*\S*)', log)
+                energy = np.array(energy)
+                energy = energy.astype(float)
+
+                for ist, en in enumerate(energy):
+                    if ist < molecule.nst - 1:
+                        molecule.states[ist + 1].energy = en
+
+        if (not calc_force_only):
+            for states in molecule.states:
+                states.force = np.zeros((molecule.nat, molecule.nsp))
+
+        # Adiabatic force 
+        tmp_f = "Gradient of\D*\s*" 
+        num_line = int(molecule.nat / 6)
+        if (num_line >= 1):
+              tmp_f += ("\s*\d*\s*\d*\s*\d*\s*\d*\s*\d*\s*\d*" 
+                     + ("\s*\d?\s*" + "([-]*\S*)\s*" * 6) * 3) * num_line
+
+        dnum = molecule.nat % 6
+        tmp_f += "\s*\d*" * dnum
+        tmp_f += ("\s*\d?\s*" + "([-]*\S*)\s*" * dnum) * 3
+
+        force = re.findall(tmp_f, log)
+        force = np.array(force)
+        force = force.astype(float)
+        force = -force
+
+        nline = 0
+        for iiter in range(num_line):
+            tmp_force = np.transpose(force[0][18 * iiter:18 * (iiter + 1)].reshape(3, 6, order="C"))
+            for iat in range(6):
+                states.force[6 * nline + iat] = tmp_force[iat]
+            nline += 1
+            if (iiter == num_line -1):
+                tmp_force = np.transpose(force[0][18 * (iiter + 1):].reshape(3, dnum, order="C"))
+                for iat in range(dnum):
+                    states.force[6 * nline + iat] = tmp_force[iat]
+
+        # Non-adiabatic coupling vector
+        tmp_nac = "with ETF[:]*\s*Atom\s*X\s*Y\s*Z\s*[-]*" + ("\s*\d*\s*" + "([-]*\S*)\s*"*3) * molecule.nat
+        nac = re.findall(tmp_nac, log)
+        nac = np.array(nac)
+        nac = nac.astype(float)
+
+        num = 0
+        for ist in range(molecule.nst):
+            for jst in range(ist + 1, molecule.nst):
+                molecule.nac[ist, jst] = nac[num].reshape(molecule.nat, 3, order='C')
+                molecule.nac[jst, ist] = -molecule.nac[ist, jst]
+                num += 1
