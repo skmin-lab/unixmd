0866ce199dfac88fd8bf9092bdb76064ced183ef
tikimtikim <tikim@unist.ac.kr>
2020-06-23 15:30:04 +0900

    make skeleton code for CTMQC

--- src/mqc/ct.py
+++ src/mqc/ct.py
@@ -2,36 +2,47 @@ from __future__ import division
 import numpy as np
 from mqc.mqc import MQC
 
-"""
-   coupled-trajectory mixed quantum-classical dynamics
-"""
 class CT(MQC):
-
-    def __init__(self, molecule, istate=1, dt=0.5, nsteps=1000, nesteps=10000, \
-        propagation="density", thermostat={"thermo":'none', "nrescale":0}, \
-        l_adjnac=True, l_deco=False ):
+    """ Class for Coupled-Trajectory Mixed Quantum-Classical dynamics (CTMQC)
+    """
+    def __init__(self, molecules, istate=0, dt=0.5, nsteps=1000, nesteps=10000, \
+        propagation="density", l_adjnac=True, threshold=1.0e-4, dist_cutoff=2.0/0.529166):
 
         # Initialize input values
-        self.l_deco = l_deco
-
-        super().__init__(molecule, istate, dt, nsteps, nesteps, \
-            propagation, thermostat, l_adjnac)
-
-        pass     
-
-    """
-       Routine related to the SH works
-    """
-    def step(self):
         pass
- 
-    """
-       Routine to calculate the forces
-    """
+
+    def run(self):
+        """ Run MQC dynamics according to CTMQC
+        """
+        pass
+
+    def calculate_qmom(self, molecules):
+        """ Routine to calculate quantum momentum
+        """
+        pass
+
     def calculate_force(self, states):
-        super().calculate_force()
+        """ Routine to calculate force
+        """
         pass
 
+    def update_energy(self, molecule):
+        """ Routine to update the energy of molecules in surface hopping
+        """
+        pass
 
+    def el_propagator(self, molecule):
+        """ Routine to propagate BO coefficients or density matrix
+        """
+        pass
 
+    def print_init(self, molecule, theory, thermostat, debug):
+        """ Routine to print the initial information of dynamics
+        """
+        pass
+
+    def print_step(self, molecule, istep, debug):
+        """ Routine to print each steps infomation about dynamics
+        """
+        pass
 
