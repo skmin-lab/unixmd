1ac39627d7732e9e4a52aa339dd1b72730a76b3d
jkha <hjk0618@unist.ac.kr>
2020-07-21 18:20:23 +0900

    Change variable name for quantum momentum force ('qmom_force to l_qmom_force')

--- src/mqc/ehxf.py
+++ src/mqc/ehxf.py
@@ -44,7 +44,7 @@ class EhXF(MQC):
         :param boolean l_adjnac: logical to adjust nonadiabatic coupling
     """
     def __init__(self, molecule, istate=0, dt=0.5, nsteps=1000, nesteps=10000, \
-        propagation="density", l_adjnac=True, threshold=0.01, wsigma=0.1, qmom_force=False):
+        propagation="density", l_adjnac=True, threshold=0.01, wsigma=0.1, l_qmom_force=False):
         # Initialize input values
         super().__init__(molecule, istate, dt, nsteps, nesteps, \
             propagation, l_adjnac)
@@ -61,7 +61,7 @@ class EhXF(MQC):
         self.upper_th = 1. - self.threshold
         self.lower_th = self.threshold
 
-        self.qmom_force = qmom_force
+        self.l_qmom_force = l_qmom_force
 
         # Initialize auxiliary molecule object
         self.aux = Auxiliary_Molecule(molecule)
@@ -175,7 +175,7 @@ class EhXF(MQC):
                 self.rforce += 2. * molecule.nac[ist, jst] * molecule.rho.real[ist, jst] \
                     * (molecule.states[ist].energy - molecule.states[jst].energy)
         
-        if (self.qmom_force):
+        if (self.l_qmom_force):
             # Calculate quantum momentum
             qmom = np.zeros((molecule.nat, molecule.nsp))
             for ist in range(molecule.nst):
