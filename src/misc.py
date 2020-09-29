fdddd5ab536523eb20c8dc1b8da8e04b0c19eb9c
tikimtikim <tikim@unist.ac.kr>
2020-09-29 13:45:04 +0900

    add missing unit conversion

--- src/misc.py
+++ src/misc.py
@@ -36,6 +36,8 @@ au_to_K = 3.15774646E+5
 
 au_to_eV = 27.2113961
 eV_to_au = 1 / au_to_eV # = 0.03674931
+au_to_kcalmol = 627.503
+kcalmol_to_au = 1 / au_to_kcalmol # = 0.00159362
 
 # Speed of light in atomic unit
 c = 137.035999108108 
