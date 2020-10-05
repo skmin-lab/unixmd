8a3a5d7e8a9bbe9a8edc67fa2aa3af2468750bb6
tikimtikim <tikim@unist.ac.kr>
2020-10-05 15:13:26 +0900

    correct adiabatic force

--- src/qm/model/ecr.py
+++ src/qm/model/ecr.py
@@ -4,7 +4,7 @@ import os, shutil, re
 import numpy as np
 
 class ECR(Model):
-    """ Class for double avoided crossing (DAC) model BO calculation
+    """ Class for extended coupling with reflection (ECR) model BO calculation
     """
     def __init__(self, molecule, A=6E-4, B=0.1, C=0.9, qm_path="./"):
         # Initialize model common variables
@@ -54,8 +54,8 @@ class ECR(Model):
         molecule.states[0].energy = 0.5 * (H[0][0] + H[1][1]) - 0.5 * sqa
         molecule.states[1].energy = 0.5 * (H[0][0] + H[1][1]) + 0.5 * sqa
 
-        molecule.states[0].force = np.dot(unitary[:, 0], np.matmul(dH, unitary[:, 0])) 
-        molecule.states[1].force = np.dot(unitary[:, 1], np.matmul(dH, unitary[:, 1])) 
+        molecule.states[0].force = np.dot(unitary[:, 1], np.matmul(dH, unitary[:, 1])) 
+        molecule.states[1].force = np.dot(unitary[:, 0], np.matmul(dH, unitary[:, 0]))
 
         molecule.nac[0, 1, 0, 0] = np.dot(unitary[:, 0], np.matmul(dH, unitary[:, 1])) / sqa
-        molecule.nac[1, 0, 0, 0] = -molecule.nac[0, 1, 0, 0]
+        molecule.nac[1, 0, 0, 0] = -np.copy(molecule.nac[0, 1, 0, 0])
