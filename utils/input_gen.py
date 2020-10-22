24d551dbb38ea6ae19d611f41f077da04cbe7cdd
swmoon-unist <swmoon@unist.ac.kr>
2020-10-22 20:13:32 +0900

    Develop input_gen.py

--- utils/input_gen.py
+++ utils/input_gen.py
@@ -1,27 +1,27 @@
-import math
 import os
+import argparse
 import shutil
 
 def main():
     #TODO: error message handling?
-    filename_1 = input("Input sampled filename (xyz format) : ")
-    f1 = open(f'{filename_1}','r')
-    lines_1 = f1.readlines()
-    f1.close()
+    parser = argparse.ArgumentParser(description = "PYTHON script for UNI-xMD input generator")
+    parser.add_argument('-s', action='store', dest='sample_XYZ', type=str, help="Directory including sampled files, sampled files must be written in xyz format", required=True)
+    parser.add_argument('-r', action='store', dest='template_run_py', type=str, help="Template for setting filename, must be written in run.py format", required=True)
+    args = parser.parse_args()
+   
+    path = os.path.join(f"./", f"{args.sample_XYZ}")
+    ntraj = len([itraj for itraj in os.listdir(path) if itraj.startswith("TRAJ_")])
+    str_traj = str(ntraj+1) 
+    index = len(str_traj) # number for trajectory indexing
     
-    natom = int(lines_1[0])
-    ntraj = int(len(lines_1)/(natom+2))
-
-    print(f"Total atom number : {natom}\n")
     print(f"Total trajectory number : {ntraj}\n")
 
-    filename_2 = input("Input setting filename (run.py format) : ")
-    f2 = open(f'{filename_2}','r')
-    lines_2 = f2.readlines()
-    f2.close()
+    readfile = open(f'{args.template_run_py}','r')
+    lines_runpy = readfile.readlines()
+    readfile.close()
 
-    for i in range(len(lines_2)):
-        tmp = lines_2[i].split()
+    for i in range(len(lines_runpy)):
+        tmp = lines_runpy[i].split()
         if (len(tmp) != 0):
             if (tmp[0] == "mol"):
                 saveline1 = i
@@ -31,37 +31,50 @@ def main():
                 if (dir_index != -1):
                     saveline2 = i
 
-    index = int(math.log10(ntraj+1))+1
-
-    for i in range(ntraj):
+    for itraj in range(ntraj):
         w = open('tmp_runpi', 'w')
-        w.write("from molecule import Molecule\n")
-        w.write("import qm, mqc\n")
-        w.write("from thermostat import *\n")
-        w.write("from misc import data\n\n")
-        w.write("geom = \"\"\"\n")
+        w.close()
 
-        for j in range(natom+2):
-            nline = i * (natom+2) + j
-            w.write(lines_1[nline])
-        w.write("\"\"\"\n")
+        with open('tmp_runpi', 'a') as f:
+            f.write("from molecule import Molecule\n")
+            f.write("import qm, mqc\n")
+            f.write("from thermostat import *\n")
+            f.write("from misc import data\n\n")
+            f.write("geom = \"\"\"\n")
+
+        traj_path = os.path.join(path,f"TRAJ_{itraj+1:0{index}d}.xyz")
+        readfile = open(f'{traj_path}','r')
+        xyz = readfile.readlines()
+        readfile.close()
+
+        if (itraj == 0):
+            natom = int(xyz[0])
+            print(f"Total atom number : {natom}\n")
+
+        for jatom in range(natom+2):
+            with open('tmp_runpi', 'a') as f:
+                f.write(xyz[jatom])
+
+        with open('tmp_runpi', 'a') as f:
+            f.write("\"\"\"\n")
 
         for j in range(saveline2-saveline1):
-            w.write(lines_2[saveline1+j])
+            with open('tmp_runpi', 'a') as f:
+                f.write(lines_runpy[saveline1+j])
 
-        savedat = lines_2[saveline2].split()
+        savedat = lines_runpy[saveline2].split()
 
         for j in range(len(savedat)):
             dir_index = savedat[j].find("input_dir")      
             if (dir_index != -1):
-                savedat[j] = f"input_dir=\"./TRAJ_{i+1:0{index}d}\", " 
-            w.write(savedat[j] + " ")
+#                savedat[j] = f"input_dir=\"./TRAJ_{i+1:0{index}d}\", " 
+                savedat[j] = f"input_dir=\"./\", " 
+            with open('tmp_runpi', 'a') as f:
+                f.write(savedat[j] + " ")
 
-        os.mkdir(f'TRAJ_{i+1:0{index}d}')
-        shutil.move("tmp_runpi", f"TRAJ_{i+1:0{index}d}")            
-        os.rename(f"TRAJ_{i+1:0{index}d}/tmp_runpi", f"TRAJ_{i+1:0{index}d}/run.py")
-
-        w.close()
+        os.mkdir(f"TRAJ_{itraj+1:0{index}d}")
+        shutil.move("tmp_runpi", f"TRAJ_{itraj+1:0{index}d}")            
+        os.rename(f"TRAJ_{itraj+1:0{index}d}/tmp_runpi", f"TRAJ_{itraj+1:0{index}d}/run.py")
          
 if __name__ == "__main__":
     main()
