b650f6591420a0f1f011034d312675b70b22d142
tikimtikim <tikim@unist.ac.kr>
2020-10-23 15:22:41 +0900

    revise total running time in SHXF-Tully1

--- examples/qm/SHXF-Tully1/run.py
+++ examples/qm/SHXF-Tully1/run.py
@@ -1,21 +1,20 @@
 from molecule import Molecule
 import qm, mqc
 from misc import data, fs_to_au
-import math
 
-data[f"X1"] = 2000
+data[f"X1"] = 2000 # au
 
 geom = """
 1
-spin-boson model
-X1  -18.56599360        0.01030671
+simple avoided crossing model
+X1       -1.64369488     0.01030334
 """
 
-mol = Molecule(geometry=geom, nsp=1, nstates=2, dof=1, unit_pos='au', unit_vel='au', model=True)
+mol = Molecule(geometry=geom, nsp=1, nstates=2, dof=1, unit_vel='au', model=True)
 
 qm = qm.model.SAC(molecule=mol)
 
 t = 0.1 / fs_to_au
-md = mqc.SHXF(molecule=mol, nsteps=23000, nesteps=1, dt=t, wsigma=0.05, istate=0, propagation="density")
+md = mqc.SHXF(molecule=mol, nsteps=10000, nesteps=1, dt=t, wsigma=0.05, istate=0, propagation="density")
 
 md.run(molecule=mol, qm=qm, input_dir=f"./", save_scr=True, save_QMlog=False)
