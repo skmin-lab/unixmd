a2fa2946831117f1813f62244032a828be5e5f43
swmoon-unist <swmoon@unist.ac.kr>
2020-11-05 15:30:19 +0900

    Add default description in input_gen.py

--- util/input_gen.py
+++ util/input_gen.py
@@ -2,18 +2,19 @@ import os
 import argparse
 import shutil
 
-def main():
+def input_gen():
     """ Python utility script for UNI-xMD input generator
         In this script, input trajectories are generated from sampled geometry files and running script
         WARNING: Sampled geom files must be named in "sample_(number).xyz"
                  running script must be read geom from "geom.xyz" file
     """
-    parser = argparse.ArgumentParser(description="Python script for UNI-xMD input generator")
+    parser = argparse.ArgumentParser(description="Python script for UNI-xMD input generator", \
+        formatter_class = argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-d', action='store', dest='sample_dir', type=str, default="Sampled", \
-        help="Directory including sampled files, sampled files must be written in extended xyz format")
+        help="Directory including sampled files, sampled files must be written in extended xyz format.")
     parser.add_argument('-f', action='store', dest='running_script', type=str, default="run.py", \
         help="Filename of personal running script, must be written in running script format. \
-        The geometry section of the running script must be read from 'geom.xyz'")
+        The geometry section of the running script must be read from 'geom.xyz'.")
     parser.add_argument('-n', action='store', dest='ntraj', type=int, \
         help="Total trajectory number", required=True)
     args = parser.parse_args()
@@ -28,7 +29,7 @@ def main():
         os.mkdir(f"TRAJ_{itraj + 1:0{index}d}")
         shutil.copy(f"{args.running_script}", f"TRAJ_{itraj + 1:0{index}d}/run.py")
         shutil.copy(f"{args.sample_dir}/sample_{itraj + 1:0{index}d}.xyz", "TRAJ_{itraj + 1:0{index}d}/geom.xyz")
-
+         
 if (__name__ == "__main__"):
-    main()
+    input_gen()
 
