a869d4769fd6972639b5a9a28bd4e2d17745c089
inseonglee <islee@unist.ac.kr>
2021-04-05 09:37:58 +0900

    Change expression for hop_rescale and hop_reject

--- src/mqc/mqc.py
+++ src/mqc/mqc.py
@@ -250,8 +250,8 @@ class MQC(object):
 
         # Print surface hopping variables
         if (self.md_type == "SH" or self.md_type == "SHXF"):
-            dynamics_info += f"\n  Velocity Rescale in Hop  = {self.hop_rescale:>16s}\n"
-            dynamics_info += f"  Rescale when Hop Reject  = {self.hop_reject:>16s}\n"
+            dynamics_info += f"\n  Rescaling after Hop      = {self.hop_rescale:>16s}\n"
+            dynamics_info += f"  Rescaling after Reject   = {self.hop_reject:>16s}\n"
 
         # Print XF variables
         if (self.md_type == "SHXF" or self.md_type == "EhXF"):
