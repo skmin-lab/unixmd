0f08b78e436133efa3749ff0ad0e6c581f5239c6
jkha <hjk0618@unist.ac.kr>
2021-04-14 11:16:35 +0900

    Add SH-Shin_Metiu example

---
+++ examples/qm/SH-Shin_Metiu/run.py
@@ -0,0 +1,33 @@
+from molecule import Molecule
+import qm, mqc
+from misc import data
+
+# For model calculation, set mass of a fictitious particle of the model.
+data[f"X1"] = 1836 # au
+
+
+# Random seed is fixed for test purpose to check if the same result is reproduced.
+import random
+random.seed(10)
+
+
+# Define the target system.
+geom = """
+1
+Shin-Metiu model
+X1       -4.0     0.0
+"""
+mol = Molecule(geometry=geom, ndim=1, nstates=2, ndof=1, unit_pos='au', l_model=True)
+
+
+# Set QM method.
+qm = qm.model.Shin_Metiu(molecule=mol)
+
+
+# Determine MD method.
+md = mqc.SH(molecule=mol, nsteps=2890, nesteps=1, dt=0.5, unit_dt='au', \
+     istate=1, elec_object="density")
+
+
+# Execute the simulation.
+md.run(qm=qm)
