c7dad65d87227b354cc7f7a7209f3e0892ebb908
jkha <hjk0618@unist.ac.kr>
2021-04-14 10:51:28 +0900

    Modify SHXF-Shin_Metiu example

--- examples/qm/SHXF-Shin_Metiu/run.py
+++ examples/qm/SHXF-Shin_Metiu/run.py
@@ -2,18 +2,32 @@ from molecule import Molecule
 import qm, mqc
 from misc import data
 
+# For model calculation, set mass of a fictitious particle of the model.
 data[f"X1"] = 1836 # au
 
+
+# Random seed is fixed for test purpose to check if the same result is reproduced.
+import random
+random.seed(10)
+
+
+# Define the target system.
 geom = """
 1
 Shin-Metiu model
 X1       -4.0     0.0
 """
+mol = Molecule(geometry=geom, ndim=1, nstates=2, ndof=1, unit_pos='au', l_model=True)
 
-mol = Molecule(geometry=geom, nsp=1, nstates=2, dof=1, unit_pos='au', model=True)
 
+# Set QM method.
 qm = qm.model.Shin_Metiu(molecule=mol)
 
-md = mqc.SHXF(molecule=mol, nsteps=2890, nesteps=1, dt=0.5, unit_dt='au', wsigma=0.1, istate=1, propagation="density")
 
-md.run(molecule=mol, qm=qm, input_dir=f"./", save_scr=True, save_QMlog=False)
+# Determine MD method.
+md = mqc.SHXF(molecule=mol, nsteps=2890, nesteps=1, dt=0.5, unit_dt='au', \
+     sigma=0.1, istate=1, elec_object="density")
+
+
+# Execute the simulation.
+md.run(qm=qm)
