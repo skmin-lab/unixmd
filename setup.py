07307a0f7c1b7e422ed6d6718985766b9c74569c
inseonglee <islee@unist.ac.kr>
2022-06-09 14:50:24 +0900

    Construct an interface for linking external libraries
    - Only GNU compilers and lapack libraries are considered.

--- setup.py
+++ setup.py
@@ -2,12 +2,32 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+import sys
 import numpy as np
+
+# Selects which math libraries to be used; Available options: None, lapack
+math_lib = None
+# Directories including the math libraries
+math_lib_dir = None
 
 sourcefile1 = ["./src/mqc/el_prop/el_propagator.pyx", "./src/mqc/el_prop/rk4.c"]
 sourcefile2 = ["./src/mqc/el_prop/el_propagator_xf.pyx", "./src/mqc/el_prop/rk4_xf.c"]
 sourcefile3 = ["./src/mqc/el_prop/el_propagator_ct.pyx", "./src/mqc/el_prop/rk4_ct.c"]
 sourcefile4 = ["./src/qm/cioverlap/cioverlap.pyx", "./src/qm/cioverlap/tdnac.c"]
+
+# External libraries to be linked
+libs = []
+# Directories for linking external libraries
+lib_dirs = []
+
+if (math_lib == None):
+    print (f"No math libraries are used!", flush=True)
+elif (math_lib == "lapack"):
+    libs += ["lapack", "refblas", "m"]
+    lib_dirs += [math_lib_dir]
+else:
+    print (f"Wrong math_lib:{math_lib} is given!", flush=True)
+    sys.exit(1)
 
 extensions = [
     Extension("el_propagator", sources=sourcefile1, include_dirs=[np.get_include()]),
