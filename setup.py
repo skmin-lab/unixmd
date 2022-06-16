19f78448e1256e9963d7eba7aa2ccb080cdee5cc
inseonglee <islee@unist.ac.kr>
2022-06-16 17:08:44 +0900

    The libraries, directories, and extra flags are considered
    - In libs, math library ('-lm') is not needed for creating dynamic library
      The '-lm' is required for making binary.
    - In extra_flags, -D option is used to set macro in C source code.
      With this option, HAVE_LAPACK can be used for preprocesses in the code.

--- setup.py
+++ setup.py
@@ -19,12 +19,15 @@ sourcefile4 = ["./src/qm/cioverlap/cioverlap.pyx", "./src/qm/cioverlap/tdnac.c"]
 libs = []
 # Directories for linking external libraries
 lib_dirs = []
+# Extra flags for compilation
+extra_flags = []
 
 if (math_lib == None):
-    print (f"No math libraries are used!", flush=True)
+    print ("No math libraries are used!", flush=True)
 elif (math_lib == "lapack"):
-    libs += ["lapack", "refblas", "m"]
+    libs += ["lapack", "refblas"]
     lib_dirs += [math_lib_dir]
+    extra_flags += ["-D HAVE_LAPACK"]
 else:
     print (f"Wrong math_lib:{math_lib} is given!", flush=True)
     sys.exit(1)
@@ -33,7 +36,8 @@ extensions = [
     Extension("el_propagator", sources=sourcefile1, include_dirs=[np.get_include()]),
     Extension("el_propagator_xf", sources=sourcefile2, include_dirs=[np.get_include()]),
     Extension("el_propagator_ct", sources=sourcefile3, include_dirs=[np.get_include()]),
-    Extension("cioverlap", sources=sourcefile4, include_dirs=[np.get_include()])
+    Extension("cioverlap", sources=sourcefile4, include_dirs=[np.get_include()], \
+        libraries=libs, library_dirs=lib_dirs, extra_compile_args=extra_flags)
 ]
 
 setup(
