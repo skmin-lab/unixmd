accefbfb6598818cdb30a393129cecdb6d3fdd97
JaeHyeokLee98 <duckhawk1@naver.com>
2024-06-12 01:01:06 +0900

    Implement FileIO class Add skeleton of fileio.py Add FileIO class in __init__.py

--- src/qm/model/__init__.py
+++ src/qm/model/__init__.py
@@ -3,3 +3,4 @@ from .sac import SAC
 from .dac import DAC
 from .ecr import ECR
 from .dag import DAG
+from .fileio import FileIO
---
+++ src/qm/model/fileio.py
@@ -0,0 +1,13 @@
+from __future__ import division
+from qm.model.model import Model
+
+class FileIO(Model):
+    """
+    """
+    def __init__(self):
+        pass
+
+    def get_data(self):
+        """
+        """
+        pass
