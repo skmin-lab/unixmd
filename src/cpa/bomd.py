66ff8a429db33c0cf9e74adf4767bba3e8087f30
JaeHyeokLee98 <duckhawk1@naver.com>
2024-06-17 14:19:36 +0900

    Copy mqc/bomd.py to cpa/bomd.py for implementing code for BOMD sampling

--- src/cpa/bomd.py
+++ src/cpa/bomd.py
@@ -29,10 +29,89 @@ class BOMD(CPA):
             error_vars = f"samp_dir = {self.samp_dir}"
             raise ValueError (f"( {self.md_type}.{call_name()} ) {error message} ( {error_vars} )")
 
-    def run(self):
-        """ Run MQC dynamics according to BOMD
+    def run(self, qm, mm=None, output_dir="./", l_save_qm_log=False, l_save_mm_log=False, l_save_scr=True, restart=None):
+        """ Run BOMD to obtain binary for CPA dynamics
+
+            :param object qm: QM object containing on-the-fly calculation information
+            :param object mm: MM object containing MM calculation information
+            :param string output_dir: Name of directory where outputs to be saved.
+            :param boolean l_save_qm_log: Logical for saving QM calculation log
+            :param boolean l_save_mm_log: Logical for saving MM calculation log
+            :param boolean l_save_scr: Logical for saving scratch directory
+            :param string restart: Option for controlling dynamics restarting
         """
-        pass
+        # Initialize PyUNIxMD
+        base_dir, unixmd_dir, qm_log_dir, mm_log_dir = \
+            self.run_init(qm, mm, output_dir, l_save_qm_log, l_save_mm_log, l_save_scr, restart)
+        bo_list = [self.istate]
+        qm.calc_coupling = False
+        qm.calc_tdp = False
+        qm.calc_tdp_grad = False
+        self.print_init(qm, mm, restart)
+
+        if (restart == None):
+            # Calculate initial input geometry at t = 0.0 s
+            self.istep = -1
+            self.mol.reset_bo(qm.calc_coupling)
+            qm.get_data(self.mol, None, base_dir, bo_list, self.dt, self.istep, calc_force_only=False)
+            if (self.mol.l_qmmm and mm != None):
+                mm.get_data(self.mol, base_dir, bo_list, self.istep, calc_force_only=False)
+            self.update_energy()
+            self.write_md_output(unixmd_dir, self.istep)
+            self.print_step(self.istep)
+
+        elif (restart == "write"):
+            # Reset initial time step to t = 0.0 s
+            self.istep = -1
+            self.write_md_output(unixmd_dir, self.istep)
+            self.print_step(self.istep)
+
+        elif (restart == "append"):
+            # Set initial time step to last successful step of previous dynamics
+            self.istep = self.fstep
+
+        self.istep += 1
+
+        # Main MD loop
+        for istep in range(self.istep, self.nsteps):
+
+            self.calculate_force()
+            self.cl_update_position(istep)
+
+            self.mol.reset_bo(qm.calc_coupling)
+            qm.get_data(self.mol, None, base_dir, bo_list, self.dt, istep, calc_force_only=False)
+            if (self.mol.l_qmmm and mm != None):
+                mm.get_data(self.mol, base_dir, bo_list, istep, calc_force_only=False)
+
+            self.calculate_force()
+            self.cl_update_velocity(istep)
+
+            if (self.thermo != None):
+                self.thermo.run(self)
+
+            self.update_energy()
+
+            if ((istep + 1) % self.out_freq == 0):
+                self.write_md_output(unixmd_dir, istep)
+                self.print_step(istep)
+            if (istep == self.nsteps - 1):
+                self.write_final_xyz(unixmd_dir, istep)
+
+            self.fstep = istep
+            restart_file = os.path.join(base_dir, "RESTART.bin")
+            with open(restart_file, 'wb') as f:
+                pickle.dump({'qm':qm, 'md':self}, f)
+
+        # Delete scratch directory
+        if (not l_save_scr):
+            tmp_dir = os.path.join(unixmd_dir, "scr_qm")
+            if (os.path.exists(tmp_dir)):
+                shutil.rmtree(tmp_dir)
+
+            if (self.mol.l_qmmm and mm != None):
+                tmp_dir = os.path.join(unixmd_dir, "scr_mm")
+                if (os.path.exists(tmp_dir)):
+                    shutil.rmtree(tmp_dir)
 
     def save_bin(self):
         """ Routine to save MD info of each step using pickle
@@ -41,20 +120,50 @@ class BOMD(CPA):
     def calculate_force(self):
         """ Routine to calculate the forces
         """
-        pass
+        self.rforce = np.copy(self.mol.states[self.istate].force)
 
     def update_energy(self):
         """ Routine to update the energy of molecules in BOMD
         """
-        pass
+        # Update kinetic energy
+        self.mol.update_kinetic()
+        self.mol.epot = self.mol.states[self.istate].energy
+        self.mol.etot = self.mol.epot + self.mol.ekin
 
-    def print_init(self):
+    def print_init(self, qm, mm, restart):
         """ Routine to print the initial information of dynamics
-        """
-        pass
 
-    def print_step(self):
-        """ Routine to print each steps information about dynamics
+            :param object qm: QM object containing on-the-fly calculation information
+            :param object mm: MM object containing MM calculation information
+            :param string restart: Option for controlling dynamics restarting
         """
-        pass
+        # Print initial information about molecule, qm, mm and thermostat
+        super().print_init(qm, mm, restart)
+
+        # Print dynamics information for start line
+        dynamics_step_info = textwrap.dedent(f"""\
+
+        {"-" * 118}
+        {"Start Dynamics":>65s}
+        {"-" * 118}
+        """)
+
+        # Print INIT for each step
+        INIT = f" #INFO{'STEP':>8s}{'State':>7s}{'Kinetic(H)':>13s}{'Potential(H)':>15s}{'Total(H)':>13s}{'Temperature(K)':>17s}"
+        dynamics_step_info += INIT
+
+        print (dynamics_step_info, flush=True)
+
+    def print_step(self, istep):
+        """ Routine to print each steps information about dynamics
+
+            :param integer istep: Current MD step
+        """
+        ctemp = self.mol.ekin * 2. / float(self.mol.ndof) * au_to_K
+
+        # Print INFO for each step
+        INFO = f" INFO{istep + 1:>9d}{self.istate:>5d} "
+        INFO += f"{self.mol.ekin:14.8f}{self.mol.epot:15.8f}{self.mol.etot:15.8f}"
+        INFO += f"{ctemp:13.6f}"
+        print (INFO, flush=True)
 
