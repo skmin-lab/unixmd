7c296a2b65b83684a217b1899813abc7a5594c41
JaeHyeokLee98 <duckhawk1@naver.com>
2024-07-03 00:29:11 +0900

    TYPO in trajectory.py remove variable nsteps in trajectory.py because it's not used make else condition and make error if cpa_index < 0

--- src/trajectory.py
+++ src/trajectory.py
@@ -7,11 +7,10 @@ class Trajectory(object):
     """ Class to save BOMD trajectory data for CPA dynamics
 
         :param object molecule: Molecule object
-        :param integer nsteps: Total step of nuclear propagation
         :param integer cpa_index: Initial index for running MQC dynamics with sampling data
         :param string samp_dir: Path of sampling data directory
     """
-    def __init__(self, molecule, nsteps, cpa_index=None, samp_dir="./"):
+    def __init__(self, molecule, cpa_index=None, samp_dir="./"):
 
         #Initialize trajectory variables
         self.cpa_index = cpa_index
@@ -20,6 +19,11 @@ class Trajectory(object):
             error_message = "cpa_index isn't defined!"
             error_vars = f"cpa_index = {self.cpa_index}"
             raise ValueError (f"( {self.md_type}.{call_name()} ) {error_message} ( {error_vars} )")
+        else:
+            if (cpa_index < 0):
+                error_message = "cpa_index shouldn't be smaller than 0"
+                error_vars = f"cpa_index = {self.cpa_index}"
+                raise ValueError (f"( {self.md_type}.{call_name()} ) {error_message} ( {error_vars} )")
         
         self.samp_dir = samp_dir
         
