856975c603650e9b8d746984fe8bbeb5f0faa3dc
JaeHyeokLee98 <duckhawk1@naver.com>
2024-11-20 17:12:23 +0900

    Add conditional statement not to run sampling with MM because it's not implemented yet

--- src/cpa/cpa.py
+++ src/cpa/cpa.py
@@ -71,6 +71,12 @@ class CPA(object):
             error_vars = f"restart = {restart}"
             raise ValueError (f"( {self.md_type}.{call_name()} ) {error_message} ( {error_vars} )")
 
+        # Run sampling with MM isn't implemented yet!
+        if (mm != None):
+            error_message = "Run sampling with MM isn't implemented yet!"
+            error_vars = f"mm = {mm}"
+            raise NotImplementedError (f"( {self.md_type}.{call_name()} ) {error_message} ( {error_vars} )")
+
         # Check compatibility of variables for QM and MM calculation
         if ((self.mol.l_qmmm and mm == None) or (not self.mol.l_qmmm and mm != None)):
             error_message = "Both logical for QM/MM and MM object is necessary!"
