4b3adf9d66bc1275fd46d624536d35c2ee2b26c1
JaeHyeokLee98 <duckhawk1@naver.com>
2024-11-20 17:16:03 +0900

    Modify cpa/cpa.py and bomd.py to define samp_dir variable which save sampling data add samp_dir variable and statements to define samp_dir in run_init method of cpa/cpa.py add code to get samp_dir variable when initialize PyUNIxMD in cpa/bomd.py

--- src/cpa/bomd.py
+++ src/cpa/bomd.py
@@ -35,7 +35,7 @@ class BOMD(CPA):
             :param string restart: Option for controlling dynamics restarting
         """
         # Initialize PyUNIxMD
-        base_dir, unixmd_dir, qm_log_dir, mm_log_dir = \
+        base_dir, unixmd_dir, samp_dir, qm_log_dir, mm_log_dir = \
             self.run_init(qm, mm, output_dir, l_save_qm_log, l_save_mm_log, l_save_scr, restart)
         bo_list = [self.istate]
         qm.calc_coupling = False
