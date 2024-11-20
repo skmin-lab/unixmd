49d20d3be9ccfc2cc648938a4b3b52cf029b197b
JaeHyeokLee98 <duckhawk1@naver.com>
2024-11-20 22:30:23 +0900

    Implement Trajectory class to read and save sampling BOMD data add src/trajectory.py to implement Trajectory class

---
+++ src/trajectory.py
@@ -0,0 +1,8 @@
+from __future__ import division
+
+class Trajectory(object)
+    """ Class to save BOMD trajectory data for CPA dynamics
+    """
+    def __init__(self):
+        # Save name of Trajectory class
+        self.traj_type = self.__class__.__name__
