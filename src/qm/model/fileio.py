0ead1c0cb9e3cdb6d331b9a60d1db95d9e5c97b5
JaeHyeokLee98 <duckhawk1@naver.com>
2025-01-13 13:26:11 +0900

    Initialize File_IO class in src/qm/model used in CPA initialize File_IO class which inherit Model class to fetch pre-calculated sampling data

---
+++ src/qm/model/fileio.py
@@ -0,0 +1,9 @@
+from __future__ import division
+from qm.model.model import Model
+
+class File_IO(Model):
+    """ Class for fetching pre-calculated sampling data used in CPA
+    """
+    def __init__(self):
+        # Initialize model common variables
+        super(File_IO, self).__init__(None)
